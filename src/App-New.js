c7bce4df1096a1b75f5d85a327ee50a33272a137
kulbir singh <kulbir2406@gmail.com>
2022-11-23 15:20:20 -0500

    updated app and app-new to work with meeting link

--- src/App-New.js
+++ src/App-New.js
@@ -1,77 +1,97 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import ZoomMtgEmbedded from '@zoomus/websdk/embedded';
+import "./App.css";
+import ZoomMtgEmbedded from "@zoomus/websdk/embedded";
 
 function App() {
-
   const client = ZoomMtgEmbedded.createClient();
 
   // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-  var signatureEndpoint = ''
+  // var signatureEndpoint = ''
   // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
-  var sdkKey = ''
-  var meetingNumber = '123456789'
-  var role = 0
-  var userName = 'React'
-  var userEmail = ''
-  var passWord = ''
+  // var sdkKey = ''
+  // var meetingNumber = '123456789'
+  // var role = 0
+  // var userName = 'React'
+  // var userEmail = ''
+  // var passWord = ''
   // pass in the registrant's token if your meeting or webinar requires registration. More info here:
   // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings#join-registered
   // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/webinars#join-registered
-  var registrantToken = ''
+  // var registrantToken = ''
+
+  const signatureEndpoint = "http://localhost:4000";
+  const sdkKey = "1YMtPEdr0zZEkZtGi2OrZ4aj7zE3r2cPmgg4";
+  const meetingNumber = "79148619137";
+  const role = 1;
+  const leaveUrl = "http://localhost:3001";
+  const userName = "React";
+  const userEmail = "kulbir24@hormail.com";
+  const passWord = "BPJfEwpBk0rrMpdFEAMF46PglhbTyB.1";
+  const registrantToken = "";
 
   function getSignature(e) {
     e.preventDefault();
 
     fetch(signatureEndpoint, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         meetingNumber: meetingNumber,
-        role: role
-      })
-    }).then(res => res.json())
-    .then(response => {
-      startMeeting(response.signature)
-    }).catch(error => {
-      console.error(error)
+        role: role,
+      }),
     })
+      .then((res) => res.json())
+      .then((response) => {
+        startMeeting(response.signature);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   function startMeeting(signature) {
-
-    let meetingSDKElement = document.getElementById('meetingSDKElement');
+    let meetingSDKElement = document.getElementById("meetingSDKElement");
 
     client.init({
       debug: true,
       zoomAppRoot: meetingSDKElement,
-      language: 'en-US',
+      language: "en-US",
       customize: {
-        meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+        meetingInfo: [
+          "topic",
+          "host",
+          "mn",
+          "pwd",
+          "telPwd",
+          "invite",
+          "participant",
+          "dc",
+          "enctype",
+        ],
         toolbar: {
           buttons: [
             {
-              text: 'Custom Button',
-              className: 'CustomButton',
+              text: "Custom Button",
+              className: "CustomButton",
               onClick: () => {
-                console.log('custom button');
-              }
-            }
-          ]
-        }
-      }
+                console.log("custom button");
+              },
+            },
+          ],
+        },
+      },
     });
 
     client.join({
-    	sdkKey: sdkKey,
-    	signature: signature,
-    	meetingNumber: meetingNumber,
-    	password: passWord,
-    	userName: userName,
+      sdkKey: sdkKey,
+      signature: signature,
+      meetingNumber: meetingNumber,
+      password: passWord,
+      userName: userName,
       userEmail: userEmail,
-      tk: registrantToken
-    })
+      tk: registrantToken,
+    });
   }
 
   return (
--- src/App.js
+++ src/App.js
@@ -1,58 +1,73 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import { ZoomMtg } from '@zoomus/websdk';
+import "./App.css";
+import { ZoomMtg } from "@zoomus/websdk";
 
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.9.0/lib', '/av');
+ZoomMtg.setZoomJSLib("https://source.zoom.us/2.9.0/lib", "/av");
 
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareWebSDK();
 // loads language files, also passes any error messages to the ui
-ZoomMtg.i18n.load('en-US');
-ZoomMtg.i18n.reload('en-US');
+ZoomMtg.i18n.load("en-US");
+ZoomMtg.i18n.reload("en-US");
 
 function App() {
-
   // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-  var signatureEndpoint = ''
+  // var signatureEndpoint = ''
   // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
-  var sdkKey = ''
-  var meetingNumber = '123456789'
-  var role = 0
-  var leaveUrl = 'http://localhost:3000'
-  var userName = 'React'
-  var userEmail = ''
-  var passWord = ''
+  // var sdkKey = ''
+  // var meetingNumber = '123456789'
+  // var role = 0
+  // var leaveUrl = 'http://localhost:3000'
+  // var userName = 'React'
+  // var userEmail = ''
+  // var passWord = ''
+  const meetingURL =
+    "https://us04web.zoom.us/j/79198330405?pwd=YYJjOsefrBMJtN94HLHa4M2iZogLjR.1";
+  const meetingId = meetingURL.split("j/")[1].split("?")[0];
+  const meetingPwd = meetingURL.split("j/")[1].split("pwd=")[1];
+
+  const signatureEndpoint = "http://localhost:4000";
+  const sdkKey = "1YMtPEdr0zZEkZtGi2OrZ4aj7zE3r2cPmgg4";
+  const meetingNumber = meetingId;
+  const role = 0;
+  const leaveUrl = "http://localhost:3001";
+  const userName = "kulbir";
+  const userEmail = "kulbir24@hormail.com";
+  const passWord = meetingPwd;
+  const registrantToken = "";
   // pass in the registrant's token if your meeting or webinar requires registration. More info here:
   // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
   // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
-  var registrantToken = ''
+  // var registrantToken = ''
 
   function getSignature(e) {
     e.preventDefault();
 
     fetch(signatureEndpoint, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         meetingNumber: meetingNumber,
-        role: role
-      })
-    }).then(res => res.json())
-    .then(response => {
-      startMeeting(response.signature)
-    }).catch(error => {
-      console.error(error)
+        role: role,
+      }),
     })
+      .then((res) => res.json())
+      .then((response) => {
+        startMeeting(response.signature);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   function startMeeting(signature) {
-    document.getElementById('zmmtg-root').style.display = 'block'
+    document.getElementById("zmmtg-root").style.display = "block";
 
     ZoomMtg.init({
       leaveUrl: leaveUrl,
       success: (success) => {
-        console.log(success)
+        console.log(success);
 
         ZoomMtg.join({
           signature: signature,
@@ -63,18 +78,17 @@ function App() {
           passWord: passWord,
           tk: registrantToken,
           success: (success) => {
-            console.log(success)
+            console.log(success);
           },
           error: (error) => {
-            console.log(error)
-          }
-        })
-
+            console.log(error);
+          },
+        });
       },
       error: (error) => {
-        console.log(error)
-      }
-    })
+        console.log(error);
+      },
+    });
   }
 
   return (
